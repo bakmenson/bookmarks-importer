32ad11c6fd553db0c32fff1a80b0b49ad52e6a7a
bakmenson <bakmenson@gmail.com>
2020-05-18 16:30:41 +0300

    added function print_help

--- bookmarks_importer.py
+++ bookmarks_importer.py
@@ -31,6 +31,26 @@ def write_bookmarks(file_path, mode, string):
             f.write(string)
 
 
+def print_help():
+    print(
+        """
+        Usage: python3 bookmarks_importer.py <command> [<arg>]
+
+        commands:
+            -w    open for writing, truncating the file first
+            -a    open for writing, appending to the end of the file if it exists
+            -h    print help
+
+            default command '-w' if no command
+
+        arg:
+            /path/to/bookmarks.html
+
+            if no arg then script will looks for bookmarks.html in script dir
+        """
+    )
+
+
 parser = BookmarksParser()
 argv_bookmarks = None
 argv_command = None
