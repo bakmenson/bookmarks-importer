d037bc0b79e59fbe6a7a13262d78edc5f6a2ad63
bakmenson <bakmenson@gmail.com>
2020-05-18 15:43:42 +0300

    added commands

--- bookmarks_importer.py
+++ bookmarks_importer.py
@@ -23,20 +23,48 @@ class BookmarksParser(HTMLParser):
         self.tag_data = []
 
 
+def write_file(file_path, mode, string):
+    with open(file_path, mode, encoding="utf-8") as f:
+        if mode == "a":
+            f.write("\n" + string)
+        else:
+            f.write(string)
+
+
 parser = BookmarksParser()
-origin_bookmarks = ""
+argv_bookmarks = None
+argv_command = None
+open_mode = str()
 
 try:
-    origin_bookmarks = argv[1]
+    argv_command = argv[1] if argv[1].startswith("-") else None
+    argv_bookmarks = argv[1] if not argv[1].startswith("-") else None
 except IndexError:
-    print("Missed path to bookmarks.html")
-    exit()
+    pass
 
 try:
-    with open(origin_bookmarks, "r", encoding="utf-8") as f:
+    argv_bookmarks = argv[2] if not argv[2].startswith("-") else None
+except IndexError:
+    pass
+
+bookmarks = argv_bookmarks if argv_bookmarks else "bookmarks.html"
+
+urls_path = "urls" if not argv_command else "urls"
+quickmarks_path = "quickmarks" if not argv_command else "quickmarks"
+
+try:
+    open_mode = "a" if "a" == argv_command.replace("-", "") else "w"
+except AttributeError:
+    open_mode = "w"
+
+print(open_mode)
+# exit()
+
+try:
+    with open(bookmarks, "r", encoding="utf-8") as f:
         parser.feed(f.read())
 except FileNotFoundError:
-    print(f"\nNo such file: '{origin_bookmarks}'"
+    print(f"\nNo such file: '{bookmarks}'"
           f"\nPlease input correct path to bookmarks.html.")
     exit()
 
@@ -50,13 +78,10 @@ if (urls_list := list(filter(lambda x: len(x) == 1, bookmarks_lst))):
 
     urls_str = "\n".join([link for sublist in urls_list for link in sublist])
 
-    with open("urls", "w", encoding="utf-8") as f:
-        f.write(urls_str)
+    write_file(urls_path, open_mode, urls_str)
 
 if (qk_list := list(filter(lambda x: len(x) == 2, bookmarks_lst))):
 
     qk_str = "\n".join([" ".join(item) for item in qk_list])
 
-    with open("quickmarks", "w", encoding="utf-8") as f:
-        f.write(qk_str)
-
+    write_file(quickmarks_path, open_mode, qk_str)
